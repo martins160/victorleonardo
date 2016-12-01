c89603d47335e0f2c41fc969c473be4fdc8f889d
Marcelo Martins <martins160@gmail.com>
2016-12-01 16:11:25 -0200

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,5 @@
-//MENU ICON
-var btnMenu = document.getElementById('btn-menu');
-var nav = document.getElementById('nav');
-btnMenu.addEventListener('click', function(){
-nav.classList.toggle('mostrar');
-});
-
-
-//PARALLAX
-$('.parallax').each(function(){
-var $obj = $(this);
-$(window).scroll(function() {
-var yPos = -($(window).scrollTop() / $obj.data('speed')); 
-var bgpos = '50% '+ yPos + 'px';
-$obj.css('background-position', bgpos );
-}); 
-});
-
-
-//ANCORA MENU    
-$(document).ready(function(){
-$('.ancora').click(function(){
-var alvo = $(this).attr('href').split('#').pop();
-$('html, body').animate({scrollTop: $('#'+alvo).offset().top }, 1000);
-return false;
-});
-});
-
-
-
 //VOLTAR AO TOPO SCROOL
-$(function() { $(window).scroll(function() { if($(this).scrollTop() != 0) { 
-$('.backtopo').fadeIn();   } else { $('.backtopo').fadeOut(); } }); 
+$(function() { $(window).scroll(function() { if($(this).scrollTop() != 0) {
+$('.backtopo').fadeIn();   } else { $('.backtopo').fadeOut(); } });
 $('.backtopo').click(function() { $('body,html').animate({scrollTop:0},800); });
 });
-
-
